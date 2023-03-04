b6b4547b354ed2d859d91828a30c49fe88f0b257
markferdinandsoriano <mfcsoriano06@gmail.com>
2023-03-05 00:52:06 +0800

    update: restructure the data for the specific table details

--- src/Pages/WeatherScreen/viewModel.ts
+++ src/Pages/WeatherScreen/viewModel.ts
@@ -1,11 +1,31 @@
 import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import convertTimezone from "../../Common/utils/convertTimezone";
+import concatKey from "../../Common/utils/concatKey";
 
 const viewModel = () => {
   const navigate = useNavigate();
   const { state } = useLocation();
 
-  console.log("state", state);
+  const [data, setData] = React.useState<Array<{ [key: string]: unknown }>>([]);
+
+  React.useEffect(() => {
+    if (JSON.stringify(state) !== "{}") {
+      const newData = {
+        date: convertTimezone(state?.timezone),
+        temp: state?.main?.temp,
+        description: concatKey(state?.weather, "description"),
+        main: concatKey(state?.weather, "main"),
+        pressure: state?.main?.pressure,
+        humidity: state?.main?.humidity,
+      };
+
+      setData([{ ...newData }]);
+    }
+    return () => {
+      setData([]);
+    };
+  }, [state]);
 
   const handleGoback = React.useCallback(() => {
     navigate("/homeScreen");
@@ -13,6 +33,7 @@ const viewModel = () => {
 
   return {
     handleGoback,
+    data,
   };
 };
 

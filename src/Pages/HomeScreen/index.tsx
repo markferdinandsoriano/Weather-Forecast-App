9288b52f2b7761475941fd17c93b599b5636504b
markferdinandsoriano <mfcsoriano06@gmail.com>
2023-03-04 20:35:29 +0800

    add textfield input for inputting the city

--- src/Pages/HomeScreen/index.tsx
+++ src/Pages/HomeScreen/index.tsx
@@ -1,3 +1,4 @@
+import TextField from "../../Components/Textfield";
 import viewModel from "./viewModel";
 
 const HomeScreen = () => {
@@ -5,13 +6,18 @@ const HomeScreen = () => {
 
   return (
     <div className=" w-full h-full flex flex-row items-start justify-center pt-[3em]">
-      <div className="w-[50%] h-full flex flex-row items-start justify-center">
+      <div className="w-[50%] h-full flex flex-col items-center justify-start gap-y-[4em]">
         <div className="flex flex-col items-center gap-y-4 mt-[1em]">
-          <p className="font-montesserat">{model?.userData["nickName"]}</p>
+          <p className="font-montesserat text-xl">
+            {model?.userData["nickName"]}
+          </p>
           <a href={model?.userData["gitHubUrl"]} target="_blank">
-            <p className="font-montesserat">{model?.userData["gitHubUrl"]}</p>
+            <p className="font-montesserat text-xl hover:font-bold">
+              {model?.userData["gitHubUrl"]}
+            </p>
           </a>
         </div>
+        <TextField />
       </div>
     </div>
   );
